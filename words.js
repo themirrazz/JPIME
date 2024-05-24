70d1403eead4f745bde834d3d0fc7b77f020d76d
themirrazz <89490659+themirrazz@users.noreply.github.com>
2024-05-24 09:06:18 -0700

    Create words.js

---
+++ words.js
@@ -0,0 +1,96 @@
+var WORDS_JP = [
+    // This, that, which
+    {
+        "sound": "kore",
+        "spelling": "これ",
+        "meaing": "This"
+    },
+    {
+        "sound": "sore",
+        "spelling": "それ",
+        "meaing": "That"
+    },
+    {
+        "sound": "are",
+        "spelling": "あれ",
+        "meaing": "That (Distant)"
+    },
+    {
+        "sound": "kono",
+        "spelling": "この",
+        "meaing": "This One"
+    },
+    {
+        "sound": "sono",
+        "spelling": "その",
+        "meaing": "That One"
+    },
+    {
+        "sound": "ano",
+        "spelling": "あの",
+        "meaing": "That One (Distant)"
+    },
+    {
+        "sound": "dono",
+        "spelling": "どの",
+        "meaing": "Which One"
+    },
+    // Here there theeere which
+    {
+        "sound": "koko",
+        "spelling": "ここ",
+        "meaning": "Here"
+    },
+    {
+        "sound": "soko",
+        "spelling": "そこ",
+        "meaning": "There"
+    },
+    {
+        "sound": "doko",
+        "spelling": "どこ",
+        "meaning": "Where"
+    },
+    // Questions
+    {
+        "sound": "nani",
+        "spelling": "何",
+        "meaning": "What"
+    },
+    {
+        "sound": "itsu",
+        "spelling": "いつ",
+        "meaning": "When"
+    },
+    {
+        "sound": "dou",
+        "spelling": "どう",
+        "meaning": "How",
+    },
+    {
+        "sound": "nande",
+        "spelling": "なんで",
+        "meaning": "Why"
+    },
+    {
+        "sound": "naze",
+        "spelling": "なぜ",
+        "meaning": "Why"
+    },
+    {
+        "sound": "dare",
+        "spelling": "誰",
+        "meaning": "Who"
+    },
+    // Yes/No
+    {
+        "sound": "hai",
+        "spelling": "はい",
+        "meaning": "Yes"
+    },
+    {
+        "sound": "iie",
+        "spelling": "いいえ",
+        "meaning": "No"
+    }
+];
